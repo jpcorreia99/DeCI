34d3f441c37d5ab6035f1a2771c7c61ba74dc093
jpcorreia99 <joao.goncalvescorreia@epfl.ch>
2021-12-29 18:06:50 +0000

    add computing interface

---
+++ peer/computing.go
@@ -0,0 +1,6 @@
+package peer
+
+// DataSharing describes functions to share data in a bittorrent-like system.
+type Computing interface {
+	SampleFunc()
+}
--- peer/mod.go
+++ peer/mod.go
@@ -13,6 +13,7 @@ type Peer interface {
 	Service
 	Messaging
 	DataSharing
+	Computing
 }
 
 // Factory is the type of function we are using to create new instances of
